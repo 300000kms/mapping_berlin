8bd732859a4c9a08040c33d9938062f8a1b1d9c1
trescientosmil <300000kms@users.noreply.github.com>
2020-03-03 11:18:33 +0100

    updated with crea

--- 3rd part.sql
+++ 3rd part.sql
@@ -12,6 +12,22 @@ select max(cast(timestamp as real)), min(cast(timestamp as real)) from flickr
 select max(cast(timestamp as real)), min(cast(timestamp as real)), owner from flickr group by owner
 
 
+select count() as c, total(cast(views as int)), owner from flickr group by owner order by c desc
+
+select count() as c, total(cast(views as int)), owner from flickr group by owner order by c desc
+
+select max(cast(timestamp as real)), min(cast(timestamp as real)), owner from flickr group by owner
+
+select avg(cast(views as real)) from flickr
+
+select max(cast(timestamp as real)) , min(cast(timestamp as real)), owner from flickr group by owner
+
+select max(cast(timestamp as real)) - min(cast(timestamp as real)), owner from flickr group by owner
+
+select (max(cast(timestamp as real)) - min(cast(timestamp as real)))/60/60/24, owner from flickr group by owner
+
+
+
 select max(cast(timestamp as real)) as max, min(cast(timestamp as real)) as min, max(cast(timestamp as real)) - min(cast(timestamp as real)) as diff, owner from flickr group by owner
 
 
@@ -31,6 +47,16 @@ create table flickro as
 select f.*, o.diff from flickr as f, owners as o
 where f.owner = o.owner
 
+
+create table flickr_diff as
+select 
+f.*, o.diff/60/60/24 as diff_day
+from flickr as f, owners as o
+where f.owner = o.owner
+
+select RecoverGeometryColumn	('flickr_diff', 'geometry', 25833, 'POINT')
+
+select CreateSpatialIndex('flickr_diff', 'geometry')
 
 
 
