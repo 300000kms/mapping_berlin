246525c8f267175c185b4961cf3d34cfde2eab6e
trescientosmil <300000kms@users.noreply.github.com>
2020-03-04 15:52:39 +0100

    Create streets_analysis.sql

---
+++ streets_analysis.sql
@@ -0,0 +1,6 @@
+create table streets_a as select st_length(geometry) length ,ST_NPoints(simplify(geometry, 10)) npoints, 
+degrees(st_azimuth(startpoint(geometry), endpoint(geometry) ) ) angle, 
+(case when degrees(st_azimuth(startpoint(geometry), endpoint(geometry) ) ) >180
+ then degrees(st_azimuth(startpoint(geometry), endpoint(geometry) ) ) -180 
+else degrees(st_azimuth(startpoint(geometry), endpoint(geometry) ) ) end) angle_s,
+geometry from streets
